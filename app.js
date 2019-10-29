a45ff11a13ece219e826eec80a866118f7b27593
Jodi N Kansagor <joedvice@gmail.com>
2019-10-28 22:38:15 -0700

    finished basic needs of lab

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ import htmlToDOM from './util/html-to-DOM.js';
 import images from './data/images.js';
 
 const hornList = document.querySelector('.horn-list');
+const hornFilter = document.querySelector('.horn-filter');
 
-images.forEach(image => {
-    const htmlImage = renderImage(image);
-    const imageDOM = htmlToDOM(htmlImage);
-    hornList.appendChild(imageDOM);
+hornFilter.addEventListener('change', () => {
+    const filter = hornFilter.value;
+    let filterImages = null;
+
+    if(!filter) {
+        filterImages = images;
+    } else {
+        filterImages = images.filter(image => {
+            return image.keyword === filter;
+        });
+    }
+    render(filterImages);
 });
 
+render(images);
+
+
+function render(imagesToRender) {
+
+    while (hornList.lastChild) {
+        hornList.lastElementChild.remove();
+    }
+
+    imagesToRender.forEach(image => {
+        const htmlImage = renderImage(image);
+        const imageDOM = htmlToDOM(htmlImage);
+        hornList.appendChild(imageDOM);
+    });
+}
