f5989572cb45417ba1be7dd736246dd769c3f46e
Jodi N Kansagor <joedvice@gmail.com>
2019-10-29 12:18:57 -0700

    created form to take in data from both dropdowns

--- app.js
+++ app.js
@@ -3,38 +3,66 @@ import htmlToDOM from './util/html-to-DOM.js';
 import images from './data/images.js';
 
 const hornList = document.querySelector('.horn-list');
-const hornFilter = document.querySelector('.horn-filter');
-const hornNumberFilter = document.querySelector('.horn-number-filter');
 
 render(images);
 
-hornFilter.addEventListener('change', () => {
-    const filter = hornFilter.value;
-    let filterImages = null;
+const userInput = document.getElementById('user-input');
 
+userInput.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(userInput);
+    const newSelection = selection(formData);
+
+    let filterImages = null;
+    const filter = newSelection;
     if (!filter) {
+        filterImages = images;
+    } else if (filter.keyword.length === 0) {
         filterImages = images;
     } else {
         filterImages = images.filter(image => {
-            return image.keyword === filter;
+            return image.keyword === filter.keyword && image.horns >= filter.hornNumber;
         });
     }
     render(filterImages);
 });
 
-hornNumberFilter.addEventListener('change', () => {
-    const numberFilter = hornNumberFilter.value;
-    let numberFilterImages = null;
+function selection(formData) {
+    const userSelection = {
+        keyword: formData.get('horn-filter'),
+        hornNumber: formData.get('horn-number-filter')
+    };
+    return userSelection;
+}
 
-    if (!numberFilter) {
-        numberFilterImages = images;
-    } else {
-        numberFilterImages = images.filter(image => {
-            return image.horns >= numberFilter;
-        });
-    }
-    render(numberFilterImages);
-});
+// hornFilter.addEventListener('change', () => {
+//     const filter = hornFilter.value;
+//     let filterImages = null;
+
+//     if (!filter) {
+//         filterImages = images;
+//     } else {
+//         filterImages = images.filter(image => {
+//             return image.keyword === filter;
+//         });
+//     }
+//     render(filterImages);
+// });
+
+// hornNumberFilter.addEventListener('change', () => {
+//     const numberFilter = hornNumberFilter.value;
+//     let numberFilterImages = null;
+
+//     if (!numberFilter) {
+//         numberFilterImages = images;
+//     } else {
+//         numberFilterImages = images.filter(image => {
+//             return image.horns >= numberFilter;
+//         });
+//     }
+//     render(numberFilterImages);
+// });
 
 function render(imagesToRender) {
 
